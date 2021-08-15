f3671027ad07d6e2eff826c551f9a58c4a8d6dab
Catherine <scrumb@my.com>
2021-08-15 11:21:25 +0300

    update LoginWKWenViewController

--- VK_Client| Zhmaeva/VK_Client | Zhmaeva_Ekaterina/TabBar/login/WKWebView/LoginWKWebViewController.swift
+++ VK_Client| Zhmaeva/VK_Client | Zhmaeva_Ekaterina/TabBar/login/WKWebView/LoginWKWebViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import WebKit
 
-class LoginWKWebViewController: UIViewController {
+final class LoginWKWebViewController: UIViewController {
 
     @IBOutlet weak var loginWebView: WKWebView! {
         didSet {
